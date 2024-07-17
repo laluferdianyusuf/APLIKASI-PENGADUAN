48240cf3e084a6c3281364456b2baf253f54c642
laluoki <laluocky450@gmail.com>
2024-07-17 09:31:38 +0700

    [backend] "add auth"

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,14 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const { JWT } = require("../lib/const");
+const { JWT, ROLES } = require("../lib/const");
 const adminController = require("../controller/adminController");
+const { admin } = require("../models");
+const { User } = require("../models");
+const userController = require("../controller/userController");
+const { where } = require("sequelize");
 
 module.exports = {
-  async authenticate(req, res) {
+  async authenticate(req, res, next) {
     const authHeader = req.get("Authorization");
     let token = " ";
     if (authHeader && authHeader.startsWith("Bearer"))
@@ -16,17 +20,17 @@ module.exports = {
         data: null,
       });
     try {
-      const { id } = jwt.verify(token, JWT.SECRET);
-      const getAdmins = await adminController.updateAdmin(id);
-      console.log(getAdmins);
-      if (!getAdmins) {
-        return res.status(401).send({
+      const { username } = jwt.verify(token, JWT.SECRET);
+      console.log("username: ", username);
+      const getUser = await User.findOne({ where: { username: username } });
+      if (!getUser) {
+        return res.status(401).json({
           status: false,
-          message: "Admin not found",
+          message: "Invalid token: User not found",
           data: null,
         });
       }
-      req.admin = getAdmins;
+      req.user = getUser;
       next();
     } catch (error) {
       console.log(error);
@@ -36,5 +40,28 @@ module.exports = {
         data: null,
       });
     }
+    const isSuperAdmin = async (req, res, next) => {
+      const admins = req.admin; // asumsikan bahwa pengguna telah diautentikasi dan objek pengguna telah disimpan di req.user
+
+      if (admins && admins.role === ROLES.SUPERADMIN) {
+        return next();
+      }
+      return res.status(401).json({
+        status: false,
+        message: "You must be superadmin to access this resource",
+        data: null,
+      });
+    };
+    const roles = async (req, res, next) => {
+      const admins = req.admin;
+
+      if (admins.role === ROLES.SUPERADMIN || admins.role === ROLES.ADMIN)
+        return next();
+      return res.status(401).send({
+        status: false,
+        message: "Your account doesn't have permissions",
+        data: null,
+      });
+    };
   },
 };
