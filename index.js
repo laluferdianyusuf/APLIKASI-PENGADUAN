6feacf82de692564474686a68869c4e196115d0d
laluoki <laluocky450@gmail.com>
2024-07-17 09:30:22 +0700

    [backend] "add some endoint"

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const authController = require("./controller/authController");
 const adminController = require("./controller/adminController");
 const middleware = require("./middleware/auth");
 const pengaduanController = require("./controller/pengaduanController");
+const userController = require("./controller/userController");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,26 +18,42 @@ app.get("/", (req, res) => {
   });
 });
 
+// Superadmin
+app.post("");
 // Admin
 // app.get("/admin", authController.authorizeAdmin);
 app.post("/admin/register", adminController.registerAdmin);
 app.post("/admin/login", adminController.loginAdmin);
-app.put("/admin/update/complaint/:id", adminController.complaintUpdate);
-app.put("/admin/done/complaint/:id", adminController.complaintDone);
 app.put(
   "/admin/update/:id",
+  middleware.authenticate,
   authController.authorizeAdmin,
   adminController.updateAdmin
 );
+app.put("/admin/update/complaint/:id", adminController.complaintUpdate);
+app.put("/admin/done/complaint/:id", adminController.complaintDone);
 
-// Complaint form client
-app.post("/klien/create", pengaduanController.complaintClient);
-app.put("/klien/update/:id", pengaduanController.updatePengaduan);
-app.put("/klien/update/status/:id", pengaduanController.complaintClient);
-app.delete("/klien/delete/:id", pengaduanController.deletePengaduan);
-app.get("/klien/getAll", pengaduanController.getAllCase);
-app.get("/klien/getCase", pengaduanController.getPengaduan);
-app.get("/klien/getGender", pengaduanController.getGender);
+// CRUD user
+app.post("/register/user", userController.registerUser);
+app.post("/login/user", userController.loginUser);
+app.put("/update/user/:id", middleware.authenticate, userController.updateUser);
+app.delete("/delete/user/:id", userController.deleteUser);
+app.get("/getAll/user/", userController.getAllUser);
+app.get("/getUserById/user/:id", userController.getUserById);
+
+// Complaint form user / CRUD items
+app.post(
+  "/create",
+  middleware.authenticate,
+  pengaduanController.complaintClient
+);
+app.put("/update/:id", pengaduanController.updatePengaduan);
+app.put("/update/status/:id", pengaduanController.complaintClient);
+app.delete("/delete/:id", pengaduanController.deletePengaduan);
+app.get("/getAll", pengaduanController.getAllCase);
+app.get("/getCase", pengaduanController.getPengaduan);
+app.get("/getCaseByUserId/:id", pengaduanController.getPengaduanByUserId);
+app.get("/getGender", pengaduanController.getGender);
 
 // complaint form Admin
 
