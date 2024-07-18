6a5267250357decef859db203f7b29761b4da87b
laluoki <laluocky450@gmail.com>
2024-07-18 12:48:53 +0700

    [backend] "updated some bug at any method"

--- controller/adminController.js
+++ controller/adminController.js
@@ -11,14 +11,15 @@ module.exports = {
       name: req.body.name,
       username: req.body.username,
       email: req.body.email,
+      address: req.body.address,
       phoneNumber: req.body.phoneNumber,
       password: await bcrypt.hash(password, JWT.SALT_ROUND),
-      role: ROLES.SUPERADMIN,
+      role: ROLES.ADMIN,
     });
-    res.status(201).json({
-      status: "Ok",
-      message: "Succesfully Register Admin",
-      data: admins,
+    return res.status(200).json({
+      status: true,
+      message: "Berhasil melakukan registrasi admin",
+      data: { admins },
     });
   },
   async loginAdmin(req, res) {
@@ -27,63 +28,80 @@ module.exports = {
       const admins = await admin.findOne({ where: { username } });
       console.log("username :", username);
       if (!admins) {
-        console.log("Email Tak Sama");
+        console.log("Belum melakukan registrasi");
         res.status(404).json({
-          message: "Can't find that account",
+          status: false,
+          message: "Akun tidak ditemukan",
+          data: null,
         });
+        return;
       }
       const isPasswordCorrect = bcrypt.compareSync(password, admins.password);
       console.log(isPasswordCorrect);
 
       if (!isPasswordCorrect) {
         console.log("Password Tak Sama ");
-        req.json({
-          message: "Can't find that Account",
+        res.status(402).json({
+          status: false,
+          message: "Akun tidak ditemukan",
+          data: null,
         });
         return;
       }
       const token = jwt.sign(
         {
-          id: admin.id,
-          name: admin.name,
+          id: admins.id,
+          name: admins.name,
+          username: admins.username,
         },
         JWT.SECRET
       );
       console.log("Generate Token ", token);
       return res.status(200).json({
         message: "Berhasil Login",
-        data: token,
+        data: { token },
       });
     } catch (error) {
       console.log(error);
+      return res.status(400).json({
+        status: false,
+        message: "Terjadi kesalah pada server",
+        data: null,
+      });
     }
   },
   async updateAdmin(req, res) {
     try {
-      const token = req.tokenPayload;
-      const id = token.id;
-      const admins = await admin.findAll({ where: { id: id } });
-      console.log("Current admin: ", admins);
-      const { name, username, email, phoneNumber } = req.body;
-      const updateAdmin = admin.update(
-        {
-          name,
-          username,
-          email,
-          phoneNumber,
-        },
-        { where: { id: id } }
-      );
-      console.log("Updated admin: ", updateAdmin);
-      res.status(201).json({
-        status: "OK",
-        message: "Admin Successfully Updated",
-        data: updateAdmin,
+      const { id } = req.params;
+      const getAdmin = await admin.findByPk(id);
+      console.log("Admin Sekarang: ", getAdmin);
+      if (!getAdmin) {
+        return res.status(404).json({
+          status: false,
+          message: "Gagal melakukan update",
+          data: null,
+        });
+      }
+      const { name, username, email, address, phoneNumber } = req.body;
+      const updateAdmin = getAdmin.update({
+        name,
+        username,
+        email,
+        address,
+        phoneNumber,
+      });
+      console.log("Admin Terbaru: ", updateAdmin);
+      return res.status(200).json({
+        status: true,
+        message: "Admin berhasil di perbarui",
+        data: { updateAdmin },
       });
     } catch (error) {
       console.log(error);
-      res.status(404).json({
-        message: "Admin Not Found",
+      return res.status(400).json({
+        status: false,
+        message: "Admin tidak di temukan",
+        data: null,
       });
     }
   },
@@ -94,24 +112,26 @@ module.exports = {
       console.log("getPengaduan: ", getPengaduan);
 
       if (!getPengaduan) {
-        return res.status(400).json({
+        return res.status(401).json({
           status: false,
-          message: "complaint not found",
+          message: "Pengaduan tidak ditemukan",
+          data: null,
         });
       }
       const updatePengaduan = await getPengaduan.update({
         status: "Sedang diproses",
       });
-      res.status(200).json({
+      return res.status(200).json({
         status: true,
-        message: "Complaint status updated to 'sedang diproses'",
-        data: updatePengaduan,
+        message: "Status pengaduan diperbarui ke 'sedang diproses'",
+        data: { updatePengaduan },
       });
     } catch (error) {
       console.log(error);
-      res.status(500).json({
+      res.status(400).json({
         status: false,
-        message: "Failed to update complaint status",
+        message: "Gagal memperbarui status pengaduan",
+        data: null,
       });
     }
   },
@@ -120,24 +140,26 @@ module.exports = {
       const { id } = req.params;
       const getPengaduan = await pengaduan.findByPk(id);
       if (!getPengaduan) {
-        return res.status(400).json({
+        return res.status(401).json({
           status: false,
-          message: "Complaint not found",
+          message: "Pengaduan tidak ditemukan",
+          data: null,
         });
       }
       const pengaduanDone = await getPengaduan.update({
         status: "Kasus Telah Selesai",
       });
-      res.status(200).json({
+      return res.status(200).json({
         status: true,
-        message: "Complaint status updated to 'Kasus Telah Selesai'",
-        data: pengaduanDone,
+        message: "Status pengaduan diperbarui ke 'Kasus Telah Selesai'",
+        data: { pengaduanDone },
       });
     } catch (error) {
       console.log(error);
-      res.status(500).json({
+      return res.status(400).json({
         status: false,
-        message: "Failed to update complaint status",
+        message: "Gagal melakukan pembaruan status pengaduan",
+        data: null,
       });
     }
   },
