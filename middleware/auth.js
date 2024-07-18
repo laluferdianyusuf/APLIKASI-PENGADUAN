9c440d4cd0482d01e726666b6a9bf18ab70f0c2c
laluoki <laluocky450@gmail.com>
2024-07-18 12:39:26 +0700

    [backend] "updated some method for authentication user and admin"

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,8 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { JWT, ROLES } = require("../lib/const");
-const adminController = require("../controller/adminController");
 const { admin } = require("../models");
 const { User } = require("../models");
-const userController = require("../controller/userController");
-const { where } = require("sequelize");
 
 module.exports = {
   async authenticate(req, res, next) {
@@ -16,17 +13,17 @@ module.exports = {
     else
       return res.status(401).send({
         status: false,
-        message: "You have to login to access this resource",
+        message: "Harus login terlebih dahulu untuk mendapatkan halaman ini",
         data: null,
       });
     try {
       const { username } = jwt.verify(token, JWT.SECRET);
-      console.log("username: ", username);
-      const getUser = await User.findOne({ where: { username: username } });
+      console.log("username : ", username);
+      const getUser = await User.findOne({ username });
       if (!getUser) {
         return res.status(401).json({
           status: false,
-          message: "Invalid token: User not found",
+          message: "Token tidak valid: Pengguna tidak ditemukan",
           data: null,
         });
       }
@@ -36,32 +33,33 @@ module.exports = {
       console.log(error);
       return res.status(401).send({
         status: false,
-        message: "Your Session has expired" + error,
+        message: "sesi telah kadaluarsa" + error,
         data: null,
       });
     }
-    const isSuperAdmin = async (req, res, next) => {
-      const admins = req.admin; // asumsikan bahwa pengguna telah diautentikasi dan objek pengguna telah disimpan di req.user
+  },
+  async isSuperAdmin(req, res, next) {
+    const admins = req.admin; // asumsikan bahwa pengguna telah diautentikasi dan objek pengguna telah disimpan di req.user
 
-      if (admins && admins.role === ROLES.SUPERADMIN) {
-        return next();
-      }
-      return res.status(401).json({
-        status: false,
-        message: "You must be superadmin to access this resource",
-        data: null,
-      });
-    };
-    const roles = async (req, res, next) => {
-      const admins = req.admin;
+    if (admins && admins.role === ROLES.SUPERADMIN) {
+      return next();
+    }
+    return res.status(401).json({
+      status: false,
+      message: "Harus menjadi superadmin untuk mengakses halaman ini",
+      data: null,
+    });
+  },
+  async roles(req, res, next) {
+    const admins = req.admin;
 
-      if (admins.role === ROLES.SUPERADMIN || admins.role === ROLES.ADMIN)
-        return next();
-      return res.status(401).send({
-        status: false,
-        message: "Your account doesn't have permissions",
-        data: null,
-      });
-    };
+    if (admins.role === ROLES.SUPERADMIN || admins.role === ROLES.ADMIN) {
+      return next();
+    }
+    return res.status(401).send({
+      status: false,
+      message: "Akun ini tidak memiliki izin",
+      data: null,
+    });
   },
 };
