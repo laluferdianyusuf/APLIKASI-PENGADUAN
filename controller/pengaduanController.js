865100ea2b23fbb635818c3ffd2d40d68ecffd96
laluferdianyusuf <lferdianyusuf@gmail.com>
2024-08-07 13:28:32 +0800

    fix method

--- controller/pengaduanController.js
+++ controller/pengaduanController.js
@@ -1,5 +1,5 @@
 const nodeMailer = require("nodemailer");
-const { pengaduan } = require("../models");
+const { pengaduan, victim, abuser } = require("../models");
 const { User } = require("../models");
 const { ROLES } = require("../lib/const");
 const { Op } = require("sequelize");
@@ -21,17 +21,17 @@ module.exports = {
       });
 
       const {
-        name,
-        born,
-        gender,
-        nik,
-        address,
-        phoneNumber,
-        education,
-        parentName,
-        parentJob,
-        parentAddress,
-        parentNumber,
+        complaintName,
+        complaintAddress,
+        complaintEducate,
+        complaintNumber,
+        complaintRelation,
+        companionName,
+        companionAddress,
+        companionEducate,
+        companionNumber,
+        companionRelation,
+        victims,
         caseType,
         caseViolence,
         physical,
@@ -39,6 +39,7 @@ module.exports = {
         psychology,
         economy,
         chronology,
+        abusers,
       } = req.body;
       if (caseViolence == "fisik" && !physical) {
         return res.status(200).json({
@@ -79,17 +80,16 @@ module.exports = {
       };
       const createPengaduan = await pengaduan.create({
         userid: user_id,
-        name,
-        born,
-        gender,
-        nik,
-        address,
-        phoneNumber,
-        education,
-        parentName,
-        parentJob,
-        parentAddress,
-        parentNumber,
+        complaintName,
+        complaintAddress,
+        complaintEducate,
+        complaintNumber,
+        complaintRelation,
+        companionName,
+        companionAddress,
+        companionEducate,
+        companionNumber,
+        companionRelation,
         caseType,
         caseViolence,
         physical,
@@ -100,6 +100,20 @@ module.exports = {
         status: "Menunggu konfirmasi",
       });
       if (createPengaduan) {
+        for (const vic of victims) {
+          await victim.create({
+            ...vic,
+            pengaduanId: createPengaduan.id,
+          });
+        }
+
+        for (const abuse of abusers) {
+          await abuser.create({
+            ...abuse,
+            pengaduanId: createPengaduan.id,
+          });
+        }
+
         await transporter.sendMail(mailOption);
         return res.status(200).json({
           status: true,
@@ -151,6 +165,7 @@ module.exports = {
         chronology,
         abusers,
       } = req.body;
+
       if (caseViolence === "fisik" && !physical) {
         return res.status(401).json({
           status: false,
@@ -229,6 +244,7 @@ module.exports = {
       });
     }
   },
+
   async deletePengaduan(req, res) {
     try {
       const user_id = req.user.id;
